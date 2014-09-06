274b17276c6acf78c63b4f929d21b030d8775124
Stephan Kleine <stephan.kleine@gmail.com>
2014-09-06 03:42:17 +0200

    overwrite files during copy

--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -8,6 +8,5 @@ cd $GOPATH/src/github.com/GoogleCloudPlatform
 git clone https://github.com/GoogleCloudPlatform/kubernetes
 cd kubernetes
 make
-rm -f /target/{apiserver,controller-manager,kubecfg,kubelet,proxy,scheduler}
-cp /opt/go/src/github.com/GoogleCloudPlatform/kubernetes/_output/go/bin/* /target
+cp -f /opt/go/src/github.com/GoogleCloudPlatform/kubernetes/_output/go/bin/* /target
 chmod +x /target/*
